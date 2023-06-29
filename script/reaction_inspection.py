61a084070f4b149f53c272d71e07685e1e16bfab
qai <qai@mit.edu>
2023-06-29 15:20:17 -0400

    i made some dummy comments

--- script/reaction_inspection.py
+++ script/reaction_inspection.py
@@ -8,4 +8,6 @@ inspect the reaction smiles from `pistachio.smi`, write out results to a `csv` f
 2. some reaction SMILES have cxsmiles (ChemAxon) annotations, some do not
 3. `Chem.Mol` objects will not be created in `Reaction.reactants/agents/products` until you run `Reaction.get_molecules`
 4. use `pandas` for creating `csv` file
+
+some dummy comments
 """
