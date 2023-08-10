8ae111b452ce4391cc8f9bc0a7f2e27727caa448
qai <qai@mit.edu>
2023-08-10 16:14:48 -0400

    fix row_id bug in updating tbl2

--- script/inspect_class_vis.py
+++ script/inspect_class_vis.py
@@ -2,7 +2,7 @@ import pickle
 
 import dash_bootstrap_components as dbc
 import pandas as pd
-from dash import Dash, Input, Output, callback, dash_table, html
+from dash import Dash, Input, Output, callback, dash_table, html, State
 
 from balancer.schema import Fragment, Reaction
 
@@ -42,9 +42,6 @@ def get_fragment_df():
     return df
 
 
-FRAGMENT_DF = get_fragment_df()
-
-
 def get_fragment_tuple_df(fragment_id: str):
     ft_list = get_fragment_tuple_list(fragment_id)
     records = []
@@ -59,31 +56,18 @@ def get_fragment_tuple_df(fragment_id: str):
     return df
 
 
-app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+FRAGMENT_DF = get_fragment_df()
+FRAGMENT_DF['id'] = FRAGMENT_DF.index
+
 TABLE1 = dash_table.DataTable(
-    FRAGMENT_DF.to_dict('records'), [{"name": i, "id": i} for i in FRAGMENT_DF.columns], id='tbl1',
+    FRAGMENT_DF.to_dict('records'),
+    columns=[{"name": i, "id": i} for i in FRAGMENT_DF.columns if i != "id"],
+    id='tbl1',
     style_data={'whiteSpace': 'normal', 'height': 'auto'},
     style_table={'height': _ROW_1_HEIGHT, 'overflowY': 'auto'}
 )
 
-TABLE2 = dash_table.DataTable([], id='tbl2', style_data={'whiteSpace': 'normal', 'height': 'auto'},
-                              style_table={'height': _ROW_1_HEIGHT, 'overflowY': 'auto'})
-
-columns_format = [
-    dict(id='svg', name='svg', presentation='markdown'),
-    dict(id='id', name='reaction_id'),
-    dict(id='smiles', name='smiles'),
-    dict(id='class_id', name='class_id'),
-    dict(id='class_name', name='class_name'),
-]
-TABLE3 = dash_table.DataTable([], id='tbl3', style_data={'whiteSpace': 'normal', 'height': 'auto'},
-                              style_table={'height': '400px', 'overflowY': 'auto'}, page_size=1, columns=columns_format,
-
-                              markdown_options={
-                                  'html': True
-                              },
-
-                              )
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 ROW1 = dbc.Row(
     [
@@ -93,17 +77,17 @@ ROW1 = dbc.Row(
             width=6,
         ),
         dbc.Col(
-            TABLE2,
             className="p-3",
-            width=6
+            width=6,
+            id="tbl2-col",
         ),
     ], className="px-3")
 
 ROW2 = dbc.Row(
     dbc.Col(
-        TABLE3,
         className="px-6",
-        width=12
+        width=12,
+        id="tbl3-col",
     ),
 )
 
@@ -112,39 +96,63 @@ app.layout = html.Div(
 )
 
 
-@callback(Output('tbl2', 'data'), Input('tbl1', 'active_cell'))
+@callback(Output('tbl2-col', 'children'), Input('tbl1', 'active_cell'))
 def update_tbl2(active_cell):
     if not active_cell:
         return []
-    i_row = active_cell['row']
+    i_row = active_cell['row_id']
     frag_id = FRAGMENT_DF.iloc[i_row, 0]
-
     df = get_fragment_tuple_df(frag_id)
-    return df.to_dict('records')
+    df['id'] = df.index
+    table2 = dash_table.DataTable(
+        df.to_dict("records"),
+        id='tbl2',
+        columns=[{"name": i, "id": i} for i in df.columns if i != 'id'],
+        style_data={'whiteSpace': 'normal', 'height': 'auto'},
+        style_table={'height': _ROW_1_HEIGHT, 'overflowY': 'auto'}
+    )
+    return table2
 
 
-@callback(Output('tbl3', 'data'), Input('tbl2', 'active_cell'), Input('tbl2', 'data'))
+@callback(Output('tbl3-col', 'children'), Input('tbl2', 'active_cell'), State('tbl2', 'data'))
 def update_reaction(active_cell, tbl2_data):
     if not active_cell:
         return []
-    i_row = active_cell['row']
+    i_row = active_cell['row_id']
     tbl2_record = tbl2_data[i_row]
     frag_tuple_repr = tbl2_record['tuple']
     frag_tuple = tuple([*frag_tuple_repr.split(" + ")])
     reactions = get_sample_reactions(frag_tuple)
-    assert get_n_reactions(frag_tuple) == tbl2_record["# of reactions"], f"{get_n_reactions(frag_tuple)}, {tbl2_record['# of reactions']}"
+    assert get_n_reactions(frag_tuple) == tbl2_record[
+        "# of reactions"], f"{get_n_reactions(frag_tuple)}, {tbl2_record['# of reactions']}"
     records = []
     for r in reactions:
         r: Reaction
         record = {
-            "id": r.identifier,
+            "svg": r.get_reaction_svg(width=_REACTION_SVG_WIDTH, height=_REACTION_SVG_HEIGHT),
             "smiles": r.reaction_smiles,
+            "identifier": r.identifier,
             "class_id": r.meta['nextmove_class_identifier'],
             "class_name": r.meta['nextmove_class'],
-            "svg": r.get_reaction_svg(width=_REACTION_SVG_WIDTH, height=_REACTION_SVG_HEIGHT)
         }
         records.append(record)
-    return records
+    df = pd.DataFrame.from_records(records)
+
+    df['id'] = df.index
+    columns_format = [{"name": i, "id": i} for i in df.columns if i != 'id']
+    for cf in columns_format:
+        if cf['name'] == 'svg':
+            cf['presentation'] = 'markdown'
+    table3 = dash_table.DataTable(
+        df.to_dict("records"),
+        id='tbl3',
+        columns=columns_format,
+        style_data={'whiteSpace': 'normal', 'height': 'auto'},
+        style_table={'height': _ROW_1_HEIGHT, 'overflowY': 'auto'},
+        markdown_options={'html': True},
+    )
+
+    return table3
 
 
 if __name__ == "__main__":
